eb6b2a2e129090888c0124cef9ee2a747255c815
Shreyas <shreyas.dbg@gmail.com>
2026-07-09 01:31:12 +0530

    Additions

--- backend/api/[...route].js
+++ backend/api/[...route].js
@@ -1,3 +1,17 @@
 import app from "../server.js";
 
-export default app;
+export default (req, res) => {
+  // Set CORS headers for Vercel serverless functions
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  
+  // Handle OPTIONS preflight requests
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  
+  return app(req, res);
+};
